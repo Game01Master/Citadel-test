4d49f7fd3bc624f1fd73a0303d5a6aed3d91f270
Game01Master <33355947+Game01Master@users.noreply.github.com>
2026-02-07 13:04:58 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -981,14 +981,14 @@ export default function App() {
         /* Global footer style */
         .app-footer {
           text-align: center;
-          padding: 8px; /* Baza je 20px sa svih strana */
+          padding: 4px; /* Baza je 20px sa svih strana */
           font-size: 14px;
           color: ${theme.subtext};
           opacity: 0.6;
         }
 
         @media (min-width: 1100px) {
-          /* Desktop verzija: Dodajemo 10px dolje (ukupno 18px) da gurnemo tekst gore */
+          /* Desktop verzija: Dodajemo 14px dolje (ukupno 18px) da gurnemo tekst gore */
           .app-footer {
             padding-bottom: 18px; 
           }
